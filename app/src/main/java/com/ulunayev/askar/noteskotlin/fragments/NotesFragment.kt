72450ce3b9ba960b39bb881a1de154d78af539d0
AnuarYespenbet <anuar.yespenbet@gmail.com>
2018-10-02 19:38:32 +0600

    //insert to db

--- app/src/main/java/com/ulunayev/askar/noteskotlin/fragments/NotesFragment.kt
+++ app/src/main/java/com/ulunayev/askar/noteskotlin/fragments/NotesFragment.kt
@@ -5,7 +5,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.ulunayev.askar.noteskotlin.R
+import kotlinx.android.synthetic.main.fragment_notes.*
 
 
 class NotesFragment: Fragment() {
@@ -14,6 +16,7 @@ class NotesFragment: Fragment() {
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
     return inflater.inflate(R.layout.fragment_notes, container, false)
+
   }
 
 
--- app/src/main/java/com/ulunayev/askar/noteskotlin/models/Note.kt
+++ app/src/main/java/com/ulunayev/askar/noteskotlin/models/Note.kt
@@ -1,5 +1,9 @@
 package com.ulunayev.askar.noteskotlin.models
 
+import java.util.*
+
 data class Note(val id: Long, var position: Int, var title: String, var note: String, val type: String)
 
+
 data class DBName(val name: String, var position: Int)
+

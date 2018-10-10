2965b492b964477b3afb9f787e9bf6d17f30b0f6
Askar <askar.ulubayev@gmail.com>
2018-10-10 22:14:33 +0600

    Repetition added

--- app/src/main/java/com/ulunayev/askar/noteskotlin/models/Note.kt
+++ app/src/main/java/com/ulunayev/askar/noteskotlin/models/Note.kt
@@ -1,8 +1,16 @@
 package com.ulunayev.askar.noteskotlin.models
 
 import com.ulunayev.askar.noteskotlin.R
+import java.util.*
 
-data class Note(var position: Int, var title: String, var note: String, val type: String, var isSelected: Boolean = false, var color: Color = Color(R.color.white, R.color.black))
+enum class Repetition{
+  NEVER, EVERY_DAY, EVERY_WEEK, EVERY_MONTH, EVERY_YEAR
+}
+
+data class Note(var position: Int, var title: String, var note: String,
+                val type: String, var isSelected: Boolean = false,
+                var color: Color = Color(R.color.white, R.color.black),
+                var time: Date = Date(), var repetition: Repetition = Repetition.NEVER)
 
 data class Section(val name: String, var position: Int)
 
